8b79c3410114d8bb3030e643f244fdaaf416550e
bhavant <bhavant.goyal@gmail.com>
2015-08-01 09:55:00 -0700

    NUnit code

---
+++ ConsoleApplication1/ClassLibrary1.Test/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary1.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(200, result);
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,41 +1,12 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
-
 class Program
 {
     public static void Main()
     {
         try
-        {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            
-            //CalculateSumOfEven();
-            //CalculateSumOfOdd();
-            //CalculateSumOfAll();
-
-            Thread T1 = new Thread(CalculateSumOfEven);
-            Thread T2 = new Thread(CalculateSumOfOdd);
-            Thread T3 = new Thread(CalculateSumOfAll);
-            T1.Start();
-            T2.Start();
-            T3.Start();
-            T1.Join();
-            T2.Join();
-            T3.Join();
-
-            //Task[] tasks = new Task[3]
-            //{
-            //    Task.Factory.StartNew(() => CalculateSumOfEven()),
-            //    Task.Factory.StartNew(() => CalculateSumOfOdd()),
-            //    Task.Factory.StartNew(() => CalculateSumOfAll())
-            //};
-            //Task.WaitAll(tasks);
-
-            watch.Stop();
-            Console.WriteLine("Time taken = {0}", watch.ElapsedMilliseconds);
+        {        
+            MathsHelper helper = new MathsHelper();
+            Console.WriteLine(helper.Add(1, 2));
         }
         catch (Exception ex)
         {
@@ -43,33 +14,12 @@ class Program
         }
         Console.ReadKey();
     }
-    public static void CalculateSumOfEven()
+}
+public class MathsHelper
+{
+    public int Add(int a, int b)
     {
-        long sum = 0;
-        for (long i = 0; i < 100000000; i++)
-        {
-            if (i % 2 == 0)
-                sum = sum + i;
-        }
-        Console.WriteLine("Sum of even numbers = {0}", sum);
-    }
-    public static void CalculateSumOfOdd()
-    {
-        long sum = 0;
-        for (long i = 0; i < 100000000; i++)
-        {
-            if (i % 2 != 0)
-                sum = sum + i;
-        }
-        Console.WriteLine("Sum of odd numbers = {0}", sum);
-    }
-    public static void CalculateSumOfAll()
-    {
-        long sum = 0;
-        for (long i = 0; i < 100000000; i++)
-        {
-            sum = sum + i;
-        }
-        Console.WriteLine("Sum of all numbers = {0}", sum);
+        int x = a + b;
+        return x;
     }
 }
